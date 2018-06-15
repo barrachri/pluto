3c86b6800ca615a6352a07028cd09e0b38c44e5f
Christian Barra <barrachri@gmail.com>
2018-06-16 00:54:02 +0200

    Switch to GH_TOKEN

--- pluto/pull_request.py
+++ pluto/pull_request.py
@@ -9,8 +9,8 @@ async def pull_request_opened_event(event, gh, *args, **kwargs):
     url = event.data["pull_request"]["comments_url"]
     author = event.data["pull_request"]["user"]["login"]
 
-    author = event.data["pull_request"]["title"]
-    author = event.data["pull_request"]["body"]
+    title = event.data["pull_request"]["title"]
+    body = event.data["pull_request"]["body"]
 
     message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
     await gh.post(url, data={"body": message})
@@ -22,14 +22,13 @@ async def delete_branch(event, gh, *args, **kwargs):
     Delete the branch once miss-islington's PR is closed.
     Say thanks if it's merged.
     """
-    if event.data["pull_request"]["user"]["login"] == "miss-islington":
-        if event.data["pull_request"]["merged"]:
-            issue_number = event.data['pull_request']['number']
-            merged_by = event.data['pull_request']['merged_by']['login']
-            if merged_by != "miss-islington":
-                await util.leave_comment(gh, issue_number, f"Thanks, @{merged_by}!")
-            else:
-                await util.leave_comment(gh, issue_number, "Thanks!")
+    if event.data["pull_request"]["merged"]:
+        issue_number = event.data['pull_request']['number']
+        merged_by = event.data['pull_request']['merged_by']['login']
+        if merged_by != "miss-islington":
+            await util.leave_comment(gh, issue_number, f"Thanks, @{merged_by}!")
+        else:
+            await util.leave_comment(gh, issue_number, "Thanks!")
 
         branch_name = event.data['pull_request']['head']['ref']
         repository = event.data['repository']['"full_name"']
