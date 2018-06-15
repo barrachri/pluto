0b090ca3a17088b30dc0ec8ce401fd9ba16255d0
Christian Barra <barrachri@gmail.com>
2018-06-15 23:50:21 +0200

    Add comment

--- pluto/__main__.py
+++ pluto/__main__.py
@@ -1,14 +1,56 @@
 import os
 
 from aiohttp import web
+import aiohttp
 
-async def main(request):
+from aiohttp import web
+
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
+
+router = routing.Router()
+
+
+async def test(request):
     return web.Response(status=200, text="Hello world!")
 
 
+@router.register("issues", action="opened")
+async def issue_opened_event(event, gh, *args, **kwargs):
+    """ Whenever an issue is opened, greet the author and say thanks."""
+
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
+
+    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
+    await gh.post(url, data={"body": message})
+
+
+async def main(request):
+    # read the GitHub webhook payload
+    body = await request.read()
+
+    # our authentication token and secret
+    oauth_token = os.environ.get("GH_AUTH")
+
+    # a representation of GitHub webhook event
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+
+    # instead of mariatta, use your own username
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "barrachri",
+                                  oauth_token=oauth_token)
+
+        # call the appropriate callback for the event
+        await router.dispatch(event, gh)
+
+    # return a "Success"
+    return web.Response(status=200)
+
 if __name__ == "__main__":
     app = web.Application()
-    app.router.add_get("/", main)
+    app.router.add_get("/test", main)
+    app.router.add_post("/", main)
     port = os.environ.get("PORT")
     if port is not None:
         port = int(port)
